fb5c2a5140bf806a7ef17371607e0fbe41208129
Yuen Hon Sing <hsyuen720@gmail.com>
2022-11-20 17:46:45 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Script of Setting up a new Mac Computer
+
+This script is used to setup a new Mac Computer. It includes all applications/environment that I need.
+
+## Prerequisite
+
+-   Login Apple Account
+-   Please comment the paid application session if the apple account has not purchased such applications.
+
+## Usage
+
+```sh
+git clone https://github.com/hsyuen720/mac-setup.git
+cd mac-setup
+sh install.sh
+```
+
+## After installation
+
+1. Modify `.zshrc` file and update the following varibale
+
+    ```
+    ZSH_THEME="powerlevel10k/powerlevel10k"
+    
+    plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+    ```
+
+2. Import iStat Menu Setting (optional)
+
+3. Setup ohmyzsh `powerlevel10k` installation (kill and open terminal app)
+
+    - type `p10k configure` if `powerlvel10k` setup does not prompt
+
+4. Set terminal theme and change it to default (in `themes` folder).
+
+5. Please add the following script into `~./zshrc` if nvm cannot add it to config file properly.
+
+    ```
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    ```
+
---
+++ install.sh
@@ -0,0 +1,82 @@
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "Start Installation..."
+
+echo "setup folder"
+mkdir $HOME/projects
+mkdir $HOME/playground
+
+echo "setup homebrew"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+brew upgrade
+
+echo "setup command line tools"
+brew install git
+brew install python3
+brew install mas
+
+# NodeJS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 16 --default # the minimum version expo-cli is 16
+npm i -g yarn
+yarn global add serve
+
+# Mobile App Environment 
+brew install watchman
+brew install cocoapods
+
+# React Native Environment
+yarn global add expo-cli
+brew tap homebrew/cask-versions
+brew install --cask zulu11 # OpenJDK distribution
+
+# Flutter Environment
+git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
+echo "\n# Export Flutter Path" >> ~/.zshrc
+echo "export PATH=\"\$PATH:$HOME/flutter/bin\"" >> ~/.zshrc
+
+echo "setup terminal configuration"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp -a $(pwd)/fonts/* ~/Library/Fonts
+
+echo "setup software"
+brew install --cask visual-studio-code
+brew install --cask postman
+brew install --cask github
+brew install --cask google-chrome
+brew install --cask signal
+brew install --cask appcleaner
+brew install --cask notion
+brew install --cask typora
+brew install --cask android-studio
+brew install --cask android-platform-tools
+brew install --cask flipper
+brew install --cask docker
+brew install --cask anydesk
+
+# ---------- Free App Store Application ---------- 
+mas install 497799835 # Xcode
+mas install 1295203466 # Microsoft Remote Desktop
+mas install 409201541 # Pages
+mas install 409203825 # Numbers
+mas install 409183694 # Keynote
+mas install 1176895641 # Spark
+mas install 1444383602 # GoodNotes 5
+mas install 1559269364 # Notion Web Clipper (Safari Web Extension)
+
+# ---------- Paid App Store Application ---------- 
+# Please comment this section when current apple account do not purchase these applications
+mas install 1319778037 # iStat Menus
+mas install 441258766 # Magnet
+
+brew cleanup
+
+echo "Done!"
